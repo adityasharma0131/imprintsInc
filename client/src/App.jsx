5218a5f844100abc409fcc76cf6e721adddfe309
adityasharma0131 <adityasharma0431@gmail.com>
2024-09-12 01:04:18 +0530

    whatsaap and scrolltotop component completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home"; // Ensure Home component is imported
+import Home from "./pages/Home";
 import CategoryPage from "./pages/CategoryPage";
 import ProductPage from "./pages/ProductPage";
 import SingleCategoryPage from "./pages/SingleCategoryPage";
+import AutoScroll from "./components/AutoScroll";
+import ScrollToTop from "./components/ScrollToTop";
+import WhatsaapButton from "./components/WhatsaapButton";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <AutoScroll />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -17,6 +21,8 @@ const App = () => {
         <Route path="/category/:categoryId" element={<SingleCategoryPage />} />
         <Route path="/products/:productId" element={<ProductPage />} />
       </Routes>
+      <ScrollToTop />
+      <WhatsaapButton phoneNumber="1212121212121" />
       <Footer />
     </BrowserRouter>
   );
