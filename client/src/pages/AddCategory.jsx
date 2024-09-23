0691c2fbad4855c9f8d492161f986117a852e5aa
adityasharma0131 <adityasharma0431@gmail.com>
2024-09-23 14:23:10 +0530

    AddCategory page completed

--- client/src/pages/AddCategory.jsx
+++ client/src/pages/AddCategory.jsx
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosArrowForward } from "react-icons/io";
-
 import { HashLink as Link } from "react-router-hash-link";
 
 const AddCategory = () => {
   return (
     <>
+      {/* Dashboard Heading */}
       <div className="admin-bx">
         <div className="dash-opr-head">
           <h1 className="dash-head">
             <Link className="dash-head1" to="/product-operation">
               Products Listing Page
             </Link>
-            <IoIosArrowForward className="arrow-icon" />
+            <IoIosArrowForward />
             <span className="dash-head2">Add Category</span>
           </h1>
+        </div>
+      </div>
+
+      {/* Add Category Form */}
+      <div className="table-row">
+        <div className="category-listing">
+          <div className="operation-header">
+            <h1 className="heading">Add new Category</h1>
+          </div>
+
+          {/* Form for adding category */}
+          <form>
+            <table className="modern-table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Operation</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <input
+                      type="text"
+                      name="category-name"
+                      className="dash-input"
+                      placeholder="Enter category name"
+                    />
+                  </td>
+                  <td>
+                    <button type="submit" className="add">
+                      Add +
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
         </div>
       </div>
     </>
