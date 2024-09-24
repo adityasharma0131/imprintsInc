b3e279c3e54ad953ca429c0d5e8ddeef2155a8ae
adityasharma0131 <adityasharma0431@gmail.com>
2024-09-24 22:38:20 +0530

    AddUsers completed

--- client/src/pages/AddUser.jsx
+++ client/src/pages/AddUser.jsx
@@ -16,6 +16,61 @@ const AddUsers = () => {
           </h1>
         </div>
       </div>
+
+      <div className="table-row">
+        <div className="category-listing">
+          <div className="operation-header">
+            <h1 className="heading">Add New User</h1>
+          </div>
+
+          {/* Form for adding a new user */}
+          <form>
+            <table className="modern-table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Password</th>
+                  <th>Operation</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <input
+                      type="text"
+                      name="name"
+                      className="dash-input"
+                      placeholder="Enter name"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="email"
+                      name="email"
+                      className="dash-input"
+                      placeholder="Enter email"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="password"
+                      name="password"
+                      className="dash-input"
+                      placeholder="Enter password"
+                    />
+                  </td>
+                  <td>
+                    <button type="submit" className="add">
+                      Add +
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
